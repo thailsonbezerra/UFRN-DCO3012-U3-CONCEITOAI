9c3cf844acc152d17e2892c71fbf69e847e2331e
Thailson Bezerra <thailsonbezerra@outlook.com>
2025-11-28 17:25:47 -0300

    feat: melhoria no schema para topicos e proposições pertecentem ao mapa. (proprosições pertecentes ao mapa, retirado os unique global)

--- app/database/002_initial_seed.sql
+++ app/database/002_initial_seed.sql
@@ -1,29 +1,42 @@
-INSERT INTO topics (name) VALUES
-('sistema solar'),
-('sol'),
-('planetas'),
-('outros corpos celestes'),
-('luas'),
-('plutão'),
-('cinturão de asteroides'),
-('planetas interiores'),
-('planetas exteriores'),
-('mercúrio'),
-('vênus'),
-('terra'),
-('marte'),
-('júpiter'),
-('saturno'),
-('urano'),
-('netuno')
+INSERT INTO maps (focus_question, topic_id_central)
+VALUES ('O que há no sistema solar?', NULL)
 RETURNING id;
 
-INSERT INTO maps (focus_question, topic_id_central) VALUES
-('O que há no sistema solar?', 1)
+INSERT INTO topics (map_id, name) VALUES
+(1, 'sistema solar'),
+(1, 'sol'),
+(1, 'planetas'),
+(1, 'outros corpos celestes'),
+(1, 'luas'),
+(1, 'plutão'),
+(1, 'cinturão de asteroides'),
+(1, 'planetas interiores'),
+(1, 'planetas exteriores'),
+(1, 'mercúrio'),
+(1, 'vênus'),
+(1, 'terra'),
+(1, 'marte'),
+(1, 'júpiter'),
+(1, 'saturno'),
+(1, 'urano'),
+(1, 'netuno')
 RETURNING id;
 
-INSERT INTO propositions (topic_id_origin, topic_id_destination, text) VALUES
-(1, 2, 'inclui'), (1, 3, 'inclui'), (1, 4, 'inclui'),
-(3, 8, 'são divididos em'), (3, 9, 'são divididos em'),
-(8, 10, 'incluem'), (8, 11, 'incluem'), (8, 12, 'incluem'), (8, 13, 'incluem'),
-(9, 14, 'incluem'), (9, 15, 'incluem'), (9, 16, 'incluem'), (9, 17, 'incluem')
+UPDATE maps
+SET topic_id_central = 1
+WHERE id = 1;
+
+INSERT INTO propositions (map_id, topic_id_origin, topic_id_destination, text) VALUES
+(1, 1, 2, 'inclui'),
+(1, 1, 3, 'inclui'),
+(1, 1, 4, 'inclui'),
+(1, 3, 8, 'são divididos em'),
+(1, 3, 9, 'são divididos em'),
+(1, 8, 10, 'incluem'),
+(1, 8, 11, 'incluem'),
+(1, 8, 12, 'incluem'),
+(1, 8, 13, 'incluem'),
+(1, 9, 14, 'incluem'),
+(1, 9, 15, 'incluem'),
+(1, 9, 16, 'incluem'),
+(1, 9, 17, 'incluem');
