591690cfffe1c836c27edd74e8d84f3563ab1162
Thailson Bezerra <thailsonbezerra@outlook.com>
2025-12-03 09:16:36 -0300

    feat: adicionado cors

--- app/main.py
+++ app/main.py
@@ -1,8 +1,17 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from app.llm.router import router as llm_router
 from app.maps.router import router as maps_router
 
 app = FastAPI(title="ConceitoAI")
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(llm_router)
 app.include_router(maps_router)
